699e463b3941820aeef942012e20e5e7ae1be1d2
Mehrdad <52593172+mehrdad987@users.noreply.github.com>
2024-09-16 16:46:47 +0200

    Update Header.jsx

--- FIRSTSTEP/Header.jsx
+++ FIRSTSTEP/Header.jsx
@@ -1,18 +1,32 @@
-import React from 'react';
+import React from 'react'; // Import React library to use JSX
 
-
+// Define a functional component named Header
 function Header() {
+    // The component returns a header element
     return (
         <header>
+            {/* Navigation bar */}
             <nav>
+                {/* Unordered list for navigation items */}
                 <ul className="nav-list">
-                    <li><button className="nav-button">Home</button></li>
-                    <li><button className="nav-button">About</button></li>
-                   
+                    {/* Each list item contains a button for navigation */}
+                    <li>
+                        <button className="nav-button">Home</button>
+                    </li>
+                    <li>
+                        
                 </ul>
             </nav>
         </header>
     );
 }
 
-export default Header
+// Export the Header component so it can be used in other files
+export default Header;
+/*Import Statement: Explains that we are importing the React library to use JSX syntax.*/
+/*Component Definition: Clarifies that we are defining a functional component called Header. */   
+/*Return Statement: Indicates that the component returns a header element. */   
+/*Navigation Bar: Describes the purpose of the <nav> element. */   
+/*Unordered List: Explains that the list is used for navigation items. */   
+/*List Items: Each list item contains a button, and the comment emphasizes that these buttons serve as navigation links.  */   
+/*Export Statement: Clarifies that we are exporting the Header component for use in other parts of the application.        */    
