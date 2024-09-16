765a2fc8290213f949a5e3d72d180247525c45c7
Mehrdad <52593172+mehrdad987@users.noreply.github.com>
2024-09-16 16:43:46 +0200

    Create Header.jsx

---
+++ FIRSTSTEP/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <ul className="nav-list">
+                    <li><button className="nav-button">Home</button></li>
+                    <li><button className="nav-button">About</button></li>
+                   
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header
